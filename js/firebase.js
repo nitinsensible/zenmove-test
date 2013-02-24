775788caff95e7a2eafaf48ed41b20431c024843
Chris Bolton <c@chrisbolton.me>
2013-02-23 16:49:10 -0800

    basic firebase auth

--- js/firebase.js
+++ js/firebase.js
@@ -1,33 +1,40 @@
 var ZM = new Firebase('https://zenmove.firebaseIO.com/');
-var user = ZM.child('users');
+var authClient = new FirebaseAuthClient(ZM, function(error, user) {
+  if (error) {
+    // an error occurred while attempting login
+    console.log(error);
+  } else if (user) {
+    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+  } else {
+    // user is logged out
+  }
+});
 
 
 $(document).ready(function () {
 
     $("#login-form").submit(function () {
+        var user = {};
         user.email = $("#email").val();
         user.password = $("#password").val();
-        var authClient = new FirebaseAuthClient(ZM);
         authClient.login('password', {
-            email: '<email@domain.com>',
-            password: '<password>;'
+          email: user.email,
+          password: user.password,
+          rememberMe: true
         });
     });
 
     $("#signup-form").submit(function () {
+        var user = {};
         user.email = $("#signup-email").val();
         user.password = $("#signup-password").val();
         user.name = $("#name").val();
         user.citystate = $("#citystate").val();
-
-        var authClient = new FirebaseAuthClient(ZM, function(error, user) {
+        authClient.createUser(user.email, user.password, function(error, user) {
           if (error) {
-            // an error occurred while attempting login
-            console.log(error);
-          } else if (user) {
-            console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+            console.log(error)
           } else {
-            // user is logged out
+            console.log('User Id: ' + user.id + ', Email: ' + user.email);
           }
         });
     });
