4e9623ad235042018c02e2d102d035c6d30668e4
nitinsensible <nitin.bhide@sensiblesoftwares.com>
2013-03-04 13:36:49 +0530

    removed models not required initially. Added various user profile types.

--- js/zmmodels.js
+++ js/zmmodels.js
@@ -1,67 +1,53 @@
 
 //Models for zenmove app, we have to sets of models, Item and ItemList
 
-var Profile = Parse.Object.extend("Profile", {
+/* Models for User Profiles.
+  Instance of User will have a attribute called 'type' to define the
+  user type and 'profile' which defines the profile for the user.
+  For example, if type is 'move' then 'profile' attribute will contain
+  an instance of MoverProfile
+*/
+var ProfileBase = Parse.Object.extend("Profile", {
+    defaults: { type:'unknown'},
 
-    defaults: {},
+    initialize: function() {}
+});
 
-    initialize: function() {},
-
+var UserProfile = ProfileBase.extend('UserProfile', {
+    defaults: { type:'user'},
+    initialize: function() {}
 });
 
 
-var Post = Parse.Object.extend("Post", {
+var MoverProfile = ProfileBase.extend('MoverProfile', {
+    defaults: { type:'mover'},
+    initialize: function() {}
+});
 
+var ScoutProfile = ProfileBase.extend('MoverProfile', {
+    defaults: { type:'mover'},
+    initialize: function() {}
+});
+
+/* Move Related Posting */
+
+var Post = Parse.Object.extend("Post", {
     defaults: {},
 
-    initialize: function() {},
-
+    initialize: function() {}
 });
 
 var PostList = Parse.Collection.extend({
-
     model: Post
-
 });
 
-var Payment = Parse.Object.extend("Payment", {
-
-    defaults: {},
-
-    initialize: function() {},
-
-});
-
-var PaymentList = Parse.Collection.extend({
-
-    model: Payment
-
-});
+/* Move Related Posting */
 
 var Message = Parse.Object.extend("Message", {
-
-    defaults: {},
-
-    initialize: function() {},
-
+    defaults: { from:null, to:null, msgText:''},
+    initialize: function() {}
 });
 
 var MessageList = Parse.Collection.extend({
-
     model: Message
-
-});
-
-var Invoice = Parse.Object.extend("Invoice", {
-
-    defaults: {},
-
-    initialize: function() {},
-
-});
-
-var InvoiceList = Parse.Collection.extend({
-
-    model: Invoice
-
 });
