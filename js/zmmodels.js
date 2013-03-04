8ae966702efc26e4312b270b4eaa4c10dc28a9e3
nitinsensible <nitin.bhide@sensiblesoftwares.com>
2013-03-04 15:49:31 +0530

    some intermediate commit. Not working yet.

--- js/zmmodels.js
+++ js/zmmodels.js
@@ -2,52 +2,135 @@
 //Models for zenmove app, we have to sets of models, Item and ItemList
 
 /* Models for User Profiles.
-  Instance of User will have a attribute called 'type' to define the
-  user type and 'profile' which defines the profile for the user.
-  For example, if type is 'move' then 'profile' attribute will contain
-  an instance of MoverProfile
+  
+  User model will have a function called 'getProfile'. Which return
+  the user profile. Profile will decide the type of user and will
+  have lot of utility functions to query various things.
 */
-var ProfileBase = Parse.Object.extend("Profile", {
-    defaults: { type:'unknown'},
+var Profile = Parse.Object.extend("Profile", {
+    defaults: { type:'unknown',
+        user:null, // user object
+        address : '',        
+        zip: null,
+        city : null,
+        state : null        
+    },
 
-    initialize: function() {}
+    initialize: function() {}    
 });
 
-var UserProfile = ProfileBase.extend('UserProfile', {
-    defaults: { type:'user'},
-    initialize: function() {}
-});
+var UserProfile = function(profile) {
+    var self = this;
+    self._profile = profile;
+        
+    /* create New Post from this user */
+    self.createNewPost = function() {        
+        var post = new ZenPost();
+        post.by = self._profile.user;
+    
+        return post
+    }
+    
+    /* create a message from this user to another user */
+    self.sendMessage = function(to, msgText) {        
+        var msg= new ZenMessage();
+        msg.by = self._profile.user;
+        msg.to = to;
+        msg.msgText = msgText;
+        
+        return msg;
+    },
+
+    self.replyToMessage=function(orgMsg, msgText) {        
+        var msg= new ZenMessage();
+        msg.to = orgMsg.from;
+        msg.msgText = msgText;
+    
+        return msg;
+    },
+
+    self.getUnreadMessages = function() {        
+        // TODO. Create a query filter here for message.    
+    },
+    
+    self.posted_listings = function() {
+        // TODO
+    },
+    
+    self.hired_helpers = function() {
+        // TODO . Return a appropriate Parse.Query
+    }
+};
 
 
-var MoverProfile = ProfileBase.extend('MoverProfile', {
-    defaults: { type:'mover'},
-    initialize: function() {}
-});
+var MoverProfile = function(profile) {
+    var self = this;
+    self._profile = profile;
+        
+}
 
-var ScoutProfile = ProfileBase.extend('MoverProfile', {
-    defaults: { type:'mover'},
-    initialize: function() {}
-});
+
+var ScoutProfile = function(profile) {
+    var self = this;
+    self._profile = profile;
+        
+}
 
 /* Move Related Posting */
 
-var Post = Parse.Object.extend("Post", {
-    defaults: {},
+var ZenPost = Parse.Object.extend("ZenPost", {
+    defaults: {
+        by:null, // Posted by whom
+        type:null, // request for packing, moving, small move or large move
+        timestamp:new Date()
+    },
 
     initialize: function() {}
 });
 
-var PostList = Parse.Collection.extend({
+var ZenPostList = Parse.Collection.extend({
     model: Post
 });
 
 /* Move Related Posting */
 
-var Message = Parse.Object.extend("Message", {
-    defaults: { from:null, to:null, msgText:''},
+var ZenMessage = Parse.Object.extend("ZenMessage", {
+    defaults: {
+        from:null,
+        to:null,
+        msgText:'',
+        isRead:false, // is this message 'read'.
+        parent:null, // Non null if this message is in response (reply to) another message
+        timestamp:new Date()
+    },
     initialize: function() {}
 });
 
 var MessageList = Parse.Collection.extend({
     model: Message
 });
+
+/* Extend Parse.User to add some utility functions*/
+
+Parse.User.prototype.getProfile = function() {
+    var self =this;
+    var profile = null;
+    if('_profile' in self) {
+        profile= self._profile;
+    }else {
+        // query the profile from the server
+        var query = new Parse.Query(Profile);
+        query.equalTo('user', self);
+        query.first({
+            success:function(obj) {
+                self._profile = obj
+            },
+            error : function(obj) {
+                // For now do nothing.
+            }
+            
+        });
+    }
+    return profile;
+}
+
